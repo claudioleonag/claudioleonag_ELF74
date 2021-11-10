1c72ad9fd89a7b935257ecbf4081db118aa78931
Claudio Leon <claudio.leon@akiyama.com.br>
2021-11-10 12:04:16 -0300

    Finished version for Lab3

--- Lab3/histogram.s
+++ Lab3/histogram.s
@@ -9,35 +9,53 @@ EightBitHistogram
   MOV R6, #0 ;seta R6 como 0
   MOV R7, #0 ;seta R6 como 0
   LDR R4, [R3]; carrega da memória o endereço primeiro valor do vetor e salva em R4
-  MUL R0,R1 
+  MUL R0,R1
+  MOV R8, R0 ; armazena o tamanho da imagem no registrador R8
   CMP R0, #0x10000 ;compara se APSR(N) = 1, é menor que 64k
-  ITT MI ; Se flag N = 1 o tamanho é menor que 64k
+  IT MI ; Se flag N = 1 o tamanho é menor que 64k
   BLMI ResetaHistograma
-  MOVMI R0, #22 ; essa linha deverá retornar o valor de pixels processados
-  POP {LR}
+  MOV R5, #0
+  MOV R0, #0
+  MOV R7, #0
+  LDR R4, [R3]; carrega da memória o endereço primeiro valor do vetor e salva em R4
+  B PreencheHistograma
+  
   
   
   ;ITT PL ;Se flag N = 0
   ;MOVPL R0, #0
   ;BXPL LR  
-  BX LR
+
                   
 ResetaHistograma: ;loop para limpar o histograma
   
-  
-  LDR R5, [R4] ;carrega da memória o valor do primeiro vetor e desloca o valor de R6  
   MOV R5, #0; zera o valor do vetor
-  STR R5, [R4]; armazena valor
+  STR R5, [R4], #2; armazena valor
   ADD R7, #1 ;soma um no registrador contador
   MOV R6, #4 ; seta em 4 bits para deslocar o vetor
-  CMP R7, #128 ;compara o valor com o tamanho do vetor/2, pois cada valor do vetor ocupa half word
-  ITT NE ; se não for igual volta ao começo do loop
-  ADDNE R4, #4 ; soma quatro para o proximo endereço
+  CMP R7, #256 ;compara o valor com o tamanho do vetor/2, pois cada valor do vetor ocupa half word
+  IT NE ; se não for igual volta ao começo do loop
+  ;ADDNE R4, #2 ; soma quatro para o proximo endereço
   BNE ResetaHistograma ;retorna o loop
+
 
   BX LR 
   
- 
+PreencheHistograma
+  
+  LDRB R5, [R2] ;carrega o valor do pixel em R5
+  LDR R6, [R4, R5, LSL #1]    ;desloca o valor do endereço de memoria para onde está o vetor histogram 
+  ADD R6, #1 ; Soma um
+  STR R6, [R4, R5, LSL #1] ; armazena o valor dentro do endereço de memória R4 + R5
+  ADD R2, #1 ;desloca um na memória
+  ADD R0, #1 ;soma um no contador de pixels
+  ADD R7, #1 ;itera contador
+  CMP R7, R8 ;compara se já varreu toda a memória
+  BNE PreencheHistograma ;reseta loop
+  POP {LR}
+  BX LR
+  
+  
   
   
   
--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <vector>
+#include "images.c"
+
 
 #define WIDTH0 4
 #define HEIGHT0 3
 
-uint8_t image0[HEIGHT0][WIDTH0] = {
+/*uint8_t image0[HEIGHT0][WIDTH0] = {
   {20, 16, 16, 18},
   {255, 255, 0, 0},
   {32, 32, 32, 32}
-};
-
+};*/
+// 0 = 2
+// 16 = 2
+// 18 = 1
+// 20 = 1
+// 32 = 4
+// 255 = 2
 extern "C" uint16_t EightBitHistogram(uint16_t width, uint16_t height, uint8_t *p_image, uint16_t *p_histogram);
 
 using namespace std;
@@ -17,14 +24,21 @@ using namespace std;
 int main() {
     uint16_t resultStatus;
     std::vector<uint16_t> histogram(256,170);
-    resultStatus = EightBitHistogram(WIDTH0,HEIGHT0, (uint8_t*)&image0, (uint16_t*)&histogram);
+    int i = 0;
+    resultStatus = EightBitHistogram(WIDTH1,HEIGTH1, (uint8_t*)&image1, (uint16_t*)&histogram);
     if (resultStatus == 0)
     {
       std::cout << "Programa falhou, estourou o valor limite de tamanho de 64k!! \n";
     } 
     else
     {
-      std::cout << "Programa funcionou com sucesso! \n";
-    } 
+      std::cout << "Sucesso! Histograma calculado e imagem dentro do tamanho permitido!" << endl;
+      for (i = 0; i < 255; i++)
+      {
+        printf("Valor na posição %d = %d\n", i, histogram[i]);
+      }
+    }
+    printf ("O valor de pixels contados foi: %d\n", resultStatus);
     return 0;
+    
 }
